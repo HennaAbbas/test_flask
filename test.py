592477bbe4c6f42674f8da6fa5587dcc6628841b
Henna <62267575+HennaAbbas@users.noreply.github.com>
2020-06-17 16:41:53 -0700

    Update test.py

--- test.py
+++ test.py
@@ -155,4 +155,5 @@ if __name__ == "__main__":
         testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
         # these make sure that some options that are not applicable
         # remain hidden from the help menu.
-        failfast=False, buffer=False, catchbreak=False)        
+        failfast=False, buffer=False, catchbreak=False)       
+#test
